c5d70e9c4b0b5a8d7af3cdb1dbfe0ae821ff3871
bm20050 <bm20050@gmail.com>
2023-03-31 22:35:24 +0900

    03.31 update

--- src/binarySearch/부분탐색_계수정렬로해결.java
+++ src/binarySearch/부분탐색_계수정렬로해결.java
@@ -4,6 +4,6 @@ import java.util.Scanner;
 
 public class 부분탐색_계수정렬로해결 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in)
+        Scanner sc = new Scanner(System.in);
     }
 }
